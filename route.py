f9a13a4cc83b218bad2aba61042c441080541c1e
Andrew Weller <andrew@weller.fun>
2025-04-13 20:42:55 -0700

    Fix midland texas in manual_route_raw

--- route.py
+++ route.py
@@ -10,40 +10,40 @@ con = sqlite3.connect("words.db")
 
 route_df = pd.read_csv('manual_route_raw.csv')
 route_graph = {}
-lines = []
+all_points = []
 for row in route_df.iterrows():
     points = row[1]['WKT'].split(' ')[1:5]
     points = [point.replace('(', '').replace(',', '').replace(')', '') for point in points]
-    points = [float(point) for point in points]
-    lines.append([np.array([points[0], points[1]]), np.array([points[2], points[3]])])
+    points = [f"{points[0]} {points[1]}", f"{points[2]} {points[3]}"]
+
+    for point in points:
+        if point not in all_points:
+            all_points.append(point)
+
+    if points[0] in route_graph.keys():
+        route_graph[points[0]].append(points[1])
+    else:
+        route_graph[points[0]] = [points[1]]
+
+    if points[1] in route_graph.keys():
+        route_graph[points[1]].append(points[0])
+    else:
+        route_graph[points[1]] = [points[0]]
 
 # validate and bring points into the correct order
-current_point = lines[0][0]
-
+current_point = all_points[0]
 path = [current_point]
+print(route_graph)
+
 done_traversing = False
 while not done_traversing:
+    connections = route_graph[current_point]
     done_traversing = True
-    i = 0
-    while i < len(lines):
-        line = lines[i]
-        next_point = None
-        if np.linalg.norm(line[0] - current_point) < 0.01:
-            next_point = line[1]
-        elif np.linalg.norm(line[1] - current_point) < 0.01:
-            next_point = line[0]
-
-        if next_point is not None:
-            del lines[i]
-            path.append(current_point)
-            current_point = next_point
+    for point in connections:
+        if point not in path:
+            path.append(point)
+            current_point = point
             done_traversing = False
-        i += 1
-
-    lines.sort(key=lambda x: x[0][0])
-    if done_traversing:
-        for line in lines:
-            print(line)
 
 print(path[-1])
 print(len(path))
